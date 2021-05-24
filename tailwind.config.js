89cfec28cc28c059da55fe1234c2f2a6acb6f4fb
Gabriel Dominguez Duran <gabrieldominguezduran@gmail.com>
2021-05-24 16:16:28 +0200

    Copy UOC boilerplate and add Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: "#f4f4f4",
+      secondary: "#111111",
+      brand: "#fdf910",
+    },
+    fontFamily: {
+      brand: ["Alfa Slab One"],
+      sans: [
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+      ],
+      serif: ["Georgia", "Times New Roman", "serif"],
+    },
     extend: {},
   },
   variants: {
